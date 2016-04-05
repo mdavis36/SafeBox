a42adabcc80442bb1c7aca9df051306714619d40
Evan Sano <evan.kenji@gmail.com>
2016-04-04 20:23:08 -0700

    Started framework for FileSystemHandler

--- SafeBox/src/core/FileSystemHandler.java
+++ SafeBox/src/core/FileSystemHandler.java
@@ -1,5 +1,46 @@
 package core;
 
-public class FileSystemHandler {
+import java.util.ArrayList;
 
+import javax.swing.JTree;
+import javax.swing.tree.TreeModel;
+
+public class FileSystemHandler {
+	private int currentIndex;
+	private TreeModel contents;
+	
+	public FileSystemHandler(){
+		
+	}
+
+	public ArrayList<Folder> search(String query){
+		//TODO: 
+		ArrayList<Folder> temp = null;
+		return temp;
+	}
+	
+	public Folder getRoot(){
+		//TODO: 
+		return new Folder();
+	}
+	
+	public Folder getNode(int Node){
+		//TODO: 
+		return new Folder();
+	}
+	
+	public Folder createFolder(){
+		//TODO: 
+		return new Folder();
+	}
+	
+	public Folder createRecord(){
+		//TODO: 
+		return new Folder();
+	}
+	
+	public boolean deleteFolder(int node){
+		//TODO: 
+		return true;
+	}
 }
