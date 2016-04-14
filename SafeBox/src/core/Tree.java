9747b13fc6a45ebc429feea5467a9480e9ba5bda
Evan Sano <evan.kenji@gmail.com>
2016-04-13 20:41:42 -0700

    Started implementation of node hashtable for search

--- SafeBox/src/core/Tree.java
+++ SafeBox/src/core/Tree.java
@@ -1,26 +1,29 @@
 package core;
 
 import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.Hashtable;
 
 public class Tree implements Serializable{
 	private static final long serialVersionUID = -3576405232059064152L;
 	private Node root;
-	
+	Hashtable<String,Node> nodeMap;
 	private int maxGlobalIndex = 0;
 	
 	//Initializes the root directory to be called Home without a parent.
 	public Tree(){
+		nodeMap = new Hashtable<String,Node>();
 		root = new Node(new Folder());
 		root.setParent(null);
 		root.getData().setName("Home");
 		root.setGlobalIndex(0);
+		nodeMap.put("home", root);
 	}
 	
 	public void addNode(Node parent, Node child){
 		maxGlobalIndex++;
 		child.setGlobalIndex(maxGlobalIndex);
 		child.setParent(parent);
-		
 		parent.addChild(child);
 	}
 	
