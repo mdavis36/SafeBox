72866120e4ab2d7c527f8f5e208eaf3708027190
Mike Davis <m_davis23@u.pacific.edu>
2016-04-27 17:50:11 -0800

    pushing

--- SafeBox/src/gui/AddRecordFieldDialog.java
+++ SafeBox/src/gui/AddRecordFieldDialog.java
@@ -3,15 +3,19 @@ package gui;
 import java.awt.Color;
 
 import javax.swing.JLabel;
+import javax.swing.JTextField;
 
 public class AddRecordFieldDialog extends CustomDialog{
 
-	JLabel test;
+	private JLabel title;
+	
+	private JTextField textField;
+	
 	
 	public AddRecordFieldDialog(Color c1, Color c2, int w, int h) {
 		super(c1, c2, w, h);
-		test = new JLabel("Testing subclass");
-		centerPanel.add(test);
+		
+		
 	}
 
 }
